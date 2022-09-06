2ef7752fb638688fe571c14deb86c9b541bc373c
Mateus Virgílio <virgilionr15@gmail.com>
2022-09-06 09:37:08 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-
+import NotificationButton from "./componentes/NotificationButton"
 
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
